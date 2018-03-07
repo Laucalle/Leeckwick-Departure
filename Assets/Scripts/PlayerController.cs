87622e2b36e9696b1471ae8a620f982a1bcb35ac
Laura Calle Caraballo <laucalle09@correo.ugr.es>
2018-03-07 18:42:44 +0100

    Initial draft. Player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    float speed;
+
+    private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Vector2 movement = new Vector2(transform.position.x + (moveHorizontal * speed), transform.position.y + (moveVertical * speed));
+        rb2d.velocity = movement*speed;
+        //rb2d.MovePosition(movement);
+        //rb2d.AddForce(movement*speed);
+        //transform.position = new Vector2(transform.position.x+(moveHorizontal*speed), transform.position.y + (moveVertical*speed));
+    }
+}
