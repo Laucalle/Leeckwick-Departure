e9450a0a84dd9a2ced2d7792a5c8925950686784
Laura Calle Caraballo <laucalle09@correo.ugr.es>
2018-03-12 17:04:32 +0100

    First working A* and basic enemy movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// TO DO: patrullas - condicion de carrera del metodo Start
+// poda A*
+// completar persecucion: el enemigo conoce la ubicacion y planifica aunque te muevas
+// Vision / oido
+// estado alerta (??)
+
 public class EnemyController : MonoBehaviour {
     int sum = 0;
     Astar myplanner;
+    bool _moving;
+    List<Vector2> _plan;
+    int _current_step;
+
     // Use this for initialization
     void Start () {
         myplanner = GetComponent<Astar>();
@@ -14,10 +24,26 @@ public class EnemyController : MonoBehaviour {
 	void Update () {
         if (sum == 0) {
             sum++;
-            List<Vector2> plan = myplanner.planToPosition(transform.position, new Vector2(0, 0));
-            Debug.Log("Plan size" + plan.Count);
-            plan.ForEach(x => Debug.Log(x));
+            _plan = myplanner.planToPosition(transform.position, new Vector2(5, 2));
+            Debug.Log("Plan size" + _plan.Count);
+            _plan.ForEach(x => Debug.Log(x));
+            _moving = true;
+            _current_step = 0;
             
+        }
+        if (_moving) {
+            Vector3 direction = ( new Vector3(_plan[_current_step].x, _plan[_current_step].y) - transform.position);
+            transform.position += direction.normalized * Time.deltaTime;
+
+            if (Vector3.Distance(new Vector3(_plan[_current_step].x, _plan[_current_step].y), transform.position) < 0.3f)
+            {
+                _current_step++;
+                if (_current_step >= _plan.Count)
+                {
+                    _moving = false;
+                    _current_step = 0;
+                }
+            }
         }
         
     }
