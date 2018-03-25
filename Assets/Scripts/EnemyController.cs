315e2e2653e2d03bfad00925840238a9e5c669ec
Laura Calle Caraballo <laucalle09@correo.ugr.es>
2018-03-25 13:28:07 +0200

    Added room attributes
    Needs debugging

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -18,7 +18,15 @@ public class EnemyController : MonoBehaviour {
     GameObject player;
     private Rigidbody2D rb2d;
     List<List<Vector2>> _patrols = null;
-    List<Vector2> _patrolPoints;
+    List<Vector2> _patrolPoints = null;
+    Room _currentRoom;
+
+    [SerializeField]
+    int _roomId;
+    [SerializeField]
+    Room _myRoom;
+    
+
 
     // Use this for initialization
     void Start () {
@@ -30,11 +38,12 @@ public class EnemyController : MonoBehaviour {
         _follow = false;
         _patrolling = true;
         _currentPatrolPlan = 0;
-	}
+
+    }
 
     void InitPatrols()
     {
-
+        _patrols = new List<List<Vector2>>();
         for(int i = 0; i < _patrolPoints.Count; i++)
         {
 
@@ -43,51 +52,70 @@ public class EnemyController : MonoBehaviour {
         }
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (_replan) {
-            _replan = false;
-            if (_patrolling)
-            {
-                if(_patrols == null) InitPatrols();
 
-                _plan = _patrols[_currentPatrolPlan];
-                _currentPatrolPlan = (_currentPatrolPlan + 1) % _patrolPoints.Count;
-            }
-
-
-            if(_follow)
-            {
-                float distance_to_target = Vector3.Distance(transform.position, player.transform.position);
-
-                //if (distance_to_target < 2.0f) distance_to_target = 0.0f;
-
-                _plan = myplanner.planToPosition(transform.position, player.transform.position, distance_to_target/2);
-                Debug.Log(_plan.Count);
-                Debug.Log("New Plan distance" + Vector2.Distance(_plan[_plan.Count - 1], new Vector2(player.transform.position.x, player.transform.position.y)));
-                _follow = true;
-                _current_step = 0;
-            }
-                
+    void PlanPatrol() {
+        if (_patrols == null || _patrolPoints == null)
+        {
+            _patrolPoints = _myRoom.GetComponent<Room>().getPatrol(_roomId);
+            InitPatrols();
         }
+
         
-        Vector3 direction = ( new Vector3(_plan[_current_step].x, _plan[_current_step].y) - transform.position);
+        _plan = _patrols[_currentPatrolPlan];
+        _currentPatrolPlan = (_currentPatrolPlan + 1) % _patrolPoints.Count;
+    }
+
+    void PlanFollow() {
+        float distance_to_target = Vector3.Distance(transform.position, player.transform.position);
+
+        //if (distance_to_target < 2.0f) distance_to_target = 0.0f;
+        _plan = myplanner.planToPosition(transform.position, player.transform.position, distance_to_target / 2);
+        Debug.Log(_plan.Count);
+        Debug.Log("New Plan distance" + Vector2.Distance(_plan[_plan.Count - 1], new Vector2(player.transform.position.x, player.transform.position.y)));
+        _follow = true;
+        _current_step = 0;
+    }
+
+    void ExecutePlanStep() {
+        
+        Vector3 direction = (new Vector3(_plan[_current_step].x, _plan[_current_step].y) - transform.position);
         //rb2d.velocity = direction * 1.0f;
         transform.position += direction.normalized * Time.deltaTime;
         
         if (Vector3.Distance(new Vector3(_plan[_current_step].x, _plan[_current_step].y), transform.position) < transform.localScale.x * GetComponent<BoxCollider2D>().size.x)
         {
-            
+
             _current_step++;
             if (_current_step >= _plan.Count)
             {
-                _plan.Clear();
+                if(_follow)_plan.Clear();
+
                 _current_step = 0;
                 _replan = true;
             }
+            else {
+                Debug.LogWarning(_current_step + " of " + _plan.Count);
+            }
         }
-        
+    }
+	// Update is called once per frame
+	void Update () {
+
+        if (_replan) {
+            _replan = false;
+            if (_patrolling)
+            {
+                PlanPatrol();
+            }
+
+            if(_follow)
+            {
+                PlanFollow();
+            }
+                
+        }
+    
+        ExecutePlanStep();    
         
     }
 
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+    [SerializeField]
+    List<Vector2> _AlertPoints;
+
+    [SerializeField]
+    List<Vector2> _Patrol_1;
+
+    [SerializeField]
+    List<Vector2> _Patrol_2;
+
+    [SerializeField]
+    List<Vector2> _Patrol_3;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public List<Vector2> getPatrol(int num) {
+        if (num == 1) { return _Patrol_1; }
+        if (num == 2) { return _Patrol_2; }
+        if (num == 3) { return _Patrol_3; }
+        return new List<Vector2>();
+    }
+    public List<Vector2> getAlertPoints() {
+        return _AlertPoints;
+    }
+}
