39d78c6a0f05f8d73694cb20f2341adc8babd8b3
GermangUgr <germangalmagro@correo.ugr.es>
2018-04-16 00:31:18 +0200

    Main Menu with some art

---
+++ Assets/Scripts/ShowMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMenu : MonoBehaviour {
+
+    bool pressed, menuActive;
+    GameObject menuPanel;
+    GameObject imagePanel;
+    Animator anim;
+    float timeTrigger;
+
+    // Use this for initialization
+    void Start () {
+
+        pressed = false;
+        menuActive = false;
+        anim = GetComponent<Animator>();
+
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            if (child.name == "MenuPanel") menuPanel = child.gameObject;
+            if (child.name == "ImagePanel") imagePanel = child.gameObject;
+        }
+
+        imagePanel.SetActive(true);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if( !pressed && Input.anyKey)
+        {
+            pressed = true;
+            anim.SetTrigger("AnyKey");
+            timeTrigger = Time.time;
+        }
+
+        if(pressed && !menuActive && Time.time - timeTrigger >= 1.8)
+        {
+            menuPanel.SetActive(true);
+            menuActive = true;
+        }
+
+	}
+}
