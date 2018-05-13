3395a11e42dc9da23728a5e6c1daab71b9de5ec8
GermangUgr <germangalmagro@correo.ugr.es>
2018-05-13 19:27:48 +0200

    Display added

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class CanvasController : MonoBehaviour {
 
-    public GameObject canvas;
+
     public GameObject fadeImage;
     public GameObject pauseMenu;
-    public GameObject camera;
+
 
     // Use this for initialization
     void Start () {
@@ -28,18 +28,15 @@ public class CanvasController : MonoBehaviour {
     public void Pause()
     {
         Time.timeScale = 0;
-        canvas.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, camera.transform.position.z + 10);
         pauseMenu.SetActive(true);
-        canvas.SetActive(true);
         fadeImage.SetActive(true);
 
     }
 
     public void Resume()
     {
-        canvas.transform.position = new Vector3(0, 0, 0);
         Time.timeScale = 1;
-        canvas.SetActive(false);
+        pauseMenu.SetActive(false);
         fadeImage.SetActive(false);
     }
 }
--- Assets/Scripts/LocalInventory.cs
+++ Assets/Scripts/LocalInventory.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LocalInventory : MonoBehaviour {
     Dictionary<ItemType, int> carrying = new Dictionary<ItemType, int>();
+    public Text display;
 	// Use this for initialization
 	void Start () {
         carrying.Add(ItemType.passport, 0);
         carrying.Add(ItemType.food, 0);
         carrying.Add(ItemType.distraction, 0);
+        UpdateDisplay();
     }
 
     void OnCollisionEnter2D(Collision2D coll)
@@ -18,11 +21,18 @@ public class LocalInventory : MonoBehaviour {
         {
             CollectableItem item = coll.gameObject.GetComponent<CollectableItem>();
             carrying[item.GetItemType()] += 1;
+
             Debug.Log("Received " + item.GetItemType());
 
             Destroy(coll.gameObject);
+            UpdateDisplay();
         }
 
+    }
+
+    void UpdateDisplay()
+    {
+        display.text = "Food: " + carrying[ItemType.food] + "\n Passports: " + carrying[ItemType.passport] + "\n Distractions: " + carrying[ItemType.distraction];
     }
 
     // Update is called once per frame
