abcd4eeab33f6e92b1c921e02dcf804900a3f309
Laura Calle Caraballo <laucalle09@correo.ugr.es>
2018-05-13 18:08:10 +0200

    added items and material for field of view

---
+++ Assets/Scripts/CollectableItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType { passport, food, distraction }
+
+public class CollectableItem : MonoBehaviour {
+    [SerializeField]
+    ItemType myType;
+
+
+    // Use this for initialization
+    public ItemType GetItemType()
+    {
+        return myType;
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+}
---
+++ Assets/Scripts/LocalInventory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalInventory : MonoBehaviour {
+    Dictionary<ItemType, int> carrying = new Dictionary<ItemType, int>();
+	// Use this for initialization
+	void Start () {
+        carrying.Add(ItemType.passport, 0);
+        carrying.Add(ItemType.food, 0);
+        carrying.Add(ItemType.distraction, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        
+        if (coll.gameObject.GetComponent<CollectableItem>() != null)
+        {
+            CollectableItem item = coll.gameObject.GetComponent<CollectableItem>();
+            carrying[item.GetItemType()] += 1;
+            Debug.Log("Received " + item.GetItemType());
+
+            Destroy(coll.gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
